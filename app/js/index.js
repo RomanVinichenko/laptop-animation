c762832ddd61fdf0f8002aad9430960dfcab6ecd
Your Name <chicagezzz@gmail.com>
2021-10-06 12:48:57 +0300

    1 var

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,11 @@
-$(function () {});
+(function () {
+    'use strict';
+
+    const laptopTop = document.querySelector('.laptop__top');
+
+    function toggleLaptop() {
+        laptopTop.classList.toggle('laptop__top--active');
+    }
+
+    laptopTop.addEventListener('click', toggleLaptop);
+})();
