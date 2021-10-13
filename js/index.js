bb8efdd9dcd38b1a4da547a95ffb75c4d6ab0a9c
Your Name <chicagezzz@gmail.com>
2021-10-13 11:50:21 +0300

    Updates

--- js/index.js
+++ js/index.js
@@ -1,4 +1,7 @@
 $(function () {
+    $('.laptop__top').on('click', function () {
+        $('.laptop__top').toggleClass('laptop__top--active');
+    });
     $('.contact__type--branding').on('click', function () {
         $('.contact__type--branding').toggleClass('contact__type--active');
     });
@@ -27,43 +30,35 @@ $(function () {
         $('.contact__budget--fifty').toggleClass('contact__type--active');
     });
 });
-(function () {
-    'use strict';
+// $('.article__wrapper').mixItUp();
 
-    const laptopTop = document.querySelector('.laptop__top');
+var mixer = mixitup('.article__wrapper');
 
-    function toggleLaptop() {
-        laptopTop.classList.toggle('laptop__top--active');
-    }
+// document.onwheel = function (event) {
+//     console.log(event);
+//     if (event.deltaY > 0) {
+//         document.getElementById('line').innerHTML = 'вниз';
+//     } else {
+//         document.getElementById('line').innerHTML = 'вверх';
+//     }
+//
+//     var speed = event.deltaY;
+//     speed = Math.abs(speed);
+//     if (speed < 75) {
+//         document.getElementById('speed').innerHTML = 'низкая';
+//     } else if (speed < 125) {
+//         document.getElementById('speed').innerHTML = 'средняя';
+//     } else if (speed < 225) {
+//         document.getElementById('speed').innerHTML = 'высокая';
+//     } else {
+//         document.getElementById('speed').innerHTML = 'очень высокая';
+//     }
+// };
 
-    laptopTop.addEventListener('click', toggleLaptop);
-})();
-
-document.onwheel = function (event) {
-    console.log(event);
-    if (event.deltaY > 0) {
-        document.getElementById('line').innerHTML = 'вниз';
-    } else {
-        document.getElementById('line').innerHTML = 'вверх';
-    }
-
-    var speed = event.deltaY;
-    speed = Math.abs(speed);
-    if (speed < 75) {
-        document.getElementById('speed').innerHTML = 'низкая';
-    } else if (speed < 125) {
-        document.getElementById('speed').innerHTML = 'средняя';
-    } else if (speed < 225) {
-        document.getElementById('speed').innerHTML = 'высокая';
-    } else {
-        document.getElementById('speed').innerHTML = 'очень высокая';
-    }
-};
-
-var left = 200;
-document.getElementById('pole').onwheel = function (event) {
-    const line = event.deltaY;
-    left = left + line;
-    document.getElementById('pole-2').style.left = left + 'px';
-    return false;
-};
+// var left = 200;
+// document.getElementById('pole').onwheel = function (event) {
+//     const line = event.deltaY;
+//     left = left + line;
+//     document.getElementById('pole-2').style.left = left + 'px';
+//     return false;
+// };
