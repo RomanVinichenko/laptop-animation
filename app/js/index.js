4b2bdaab23a7075b6ea6c896cf34fa03df5d129c
Roman Vinichenko <chicagezzz@gmail.com>
2021-10-25 13:44:08 +0300

    hover effects

--- app/js/index.js
+++ app/js/index.js
@@ -1,30 +1,31 @@
 $(function () {
-    $('.contact__type--branding').on('click', function () {
-        $('.contact__type--branding').toggleClass('contact__type--active');
+    $('.contact__type-branding').on('click', function () {
+        $('.contact__type-branding').toggleClass('contact__type--active');
     });
-    $('.contact__type--illustrations').on('click', function () {
-        $('.contact__type--illustrations').toggleClass('contact__type--active');
+    $('.contact__type-illustrations').on('click', function () {
+        $('.contact__type-illustrations').toggleClass('contact__type--active');
     });
-    $('.contact__type--web').on('click', function () {
-        $('.contact__type--web').toggleClass('contact__type--active');
+    $('.contact__type-web').on('click', function () {
+        $('.contact__type-web').toggleClass('contact__type--active');
     });
-    $('.contact__type--html').on('click', function () {
-        $('.contact__type--html').toggleClass('contact__type--active');
+    $('.contact__type-html').on('click', function () {
+        $('.contact__type-html').toggleClass('contact__type--active');
     });
-    $('.contact__budget--ten').on('click', function () {
-        $('.contact__budget--ten').toggleClass('contact__type--active');
+
+    $('.contact__budget-ten').on('click', function () {
+        $('.contact__budget-ten').toggleClass('contact__type--active');
     });
-    $('.contact__budget--twelve').on('click', function () {
-        $('.contact__budget--twelve').toggleClass('contact__type--active');
+    $('.contact__budget-twelve').on('click', function () {
+        $('.contact__budget-twelve').toggleClass('contact__type--active');
     });
-    $('.contact__budget--thirty').on('click', function () {
-        $('.contact__budget--thirty').toggleClass('contact__type--active');
+    $('.contact__budget-thirty').on('click', function () {
+        $('.contact__budget-thirty').toggleClass('contact__type--active');
     });
-    $('.contact__budget--forty').on('click', function () {
-        $('.contact__budget--forty').toggleClass('contact__type--active');
+    $('.contact__budget-forty').on('click', function () {
+        $('.contact__budget-forty').toggleClass('contact__type--active');
     });
-    $('.contact__budget--fifty').on('click', function () {
-        $('.contact__budget--fifty').toggleClass('contact__type--active');
+    $('.contact__budget-fifty').on('click', function () {
+        $('.contact__budget-fifty').toggleClass('contact__type--active');
     });
     $('.links__button').on('click', function () {
         $('.links__list').toggleClass('links__list--active');
